89212e21a16ea0e08cef424f9fc1f80e0fb0b25b
Nigar Mohubaliyeva <nigarmohubaliyeva@gmail.com>
2023-08-03 22:25:51 +0300

    💄 updated the UI

--- src/App.js
+++ src/App.js
@@ -243,6 +243,16 @@ function MovieDetails({ selectedId, onCloseMovie,
     }
     getMovieDetails()
   }, [selectedId])
+
+  useEffect(function () {
+    if (!title) return;
+    document.title = `Movie | ${title}`
+
+    return function () {
+      document.title = "usePopcorn";
+    };
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? <Loader /> :
